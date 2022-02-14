b62edfa259516a1d24662f28b880ae137eb1dae9
Neyarune <99494973+Neyarune@users.noreply.github.com>
2022-02-15 02:52:08 +0300

    Add files via upload

--- batman/js/main.js
+++ batman/js/main.js
@@ -5,4 +5,43 @@ const swiper = new Swiper('.main-slider', {
     navigation: {
         nextEl: '.main-slider__arrow',
     },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        }
+    }
 });
+
+let burger = document.querySelector('.menu-burger');
+let closeMenu = document.querySelector('.header__menu-close');
+let menuMobile = document.querySelector('.header');
+
+burger.addEventListener('click', () => {
+    menuMobile.style.display = 'block';
+})
+
+closeMenu.addEventListener('click', () => {
+    menuMobile.style.display = 'none';
+})
+
+const modalWindow = document.querySelector('.modal');
+const buttonModal = document.querySelector('.main__button-play');
+
+buttonModal.addEventListener('click', (e) => {
+    modalWindow.classList.add('active')
+})
+
+modalWindow.addEventListener('click', (e) => {
+    const isModal = e.target.closest('.modal__inner');
+
+    if (!isModal) {
+        modalWindow.classList.remove('active');
+        jQuery("iframe").each(function() {
+            jQuery(this)[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+
+        })
+    }
+})
